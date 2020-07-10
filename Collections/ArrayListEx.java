49989dd9d2ed64958be08c7176d044c48f43ea0d
live1-git-ayushi <ayushi247001@gmail.com>
2020-07-10 12:24:52 +0530

    added new package

---
+++ Collections/ArrayListEx.java
@@ -0,0 +1,20 @@
+package java7.Collections;
+
+import java.util.ArrayList;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		start();
+	}
+	
+	private static void start()
+	{
+		ArrayList players = new ArrayList();
+		players.add("Rambo");
+		players.add("null");
+		players.add("Peter");
+		System.out.println(players);
+		
+	}
+}
