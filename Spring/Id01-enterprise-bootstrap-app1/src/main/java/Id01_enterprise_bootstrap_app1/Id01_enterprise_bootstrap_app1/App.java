c2295f857e32fd91cdbdfda9a36a816fd4a35030
live1-git-ayushi <ayushi247001@gmail.com>
2020-08-02 17:38:26 +0530

    updated

---
+++ Spring/Id01-enterprise-bootstrap-app1/src/main/java/Id01_enterprise_bootstrap_app1/Id01_enterprise_bootstrap_app1/App.java
@@ -0,0 +1,13 @@
+package Id01_enterprise_bootstrap_app1.Id01_enterprise_bootstrap_app1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
